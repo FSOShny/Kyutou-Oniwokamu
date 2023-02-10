84b845a13000677e1ecf96b4b229cecec11c1cce
Sh-ny-FSO <113690929+Sh-ny-FSO@users.noreply.github.com>
2023-02-11 00:02:20 +0900

    コンソールアプリ、アップデート完了しました。
    今のところは追加予定はないため。

--- Kyutou Oniwokamu/Demon.h
+++ Kyutou Oniwokamu/Demon.h
@@ -7,14 +7,12 @@ public:
 	// 鬼クラスのコンストラクタ
 	Demon();
 
+	// （キャラクタークラスメソッドのオーバーライド）
 	void Setter(int hp, int ap) override;
+	int Attack(double rate) override;
 	int Damaged(int damage) override;
 	bool CheckDead() override;
 
-	// 鬼が攻撃を行うときの処理
-	int Attack();
-	// 鬼が強攻撃を行うときの処理
-	int HeavyAtk();
 	// 鬼が激怒状態かどうかを確認する処理
 	bool CheckEnraged();
 	// 鬼が毒状態になるときの処理
@@ -24,10 +22,10 @@ public:
 	// 鬼の毒状態が消えたかどうかを確認する処理
 	bool RemovePoisoned();
 private:
-	// 毒状態の残りターン数
-	int poisonedTurn;
 	// 鬼の激怒状態を表すフラグ定数
 	const unsigned int isEnraged = (1 << 1);
 	// 鬼の毒状態を表すフラグ定数
 	const unsigned int isPoisoned = (1 << 2);
+	// 毒状態の残りターン数
+	int poisonedTurn;
 };
--- Kyutou Oniwokamu/Peach.h
+++ Kyutou Oniwokamu/Peach.h
@@ -7,18 +7,14 @@ public:
 	// 桃クラスのコンストラクタ
 	Peach();
 
+	// （キャラクタークラスメソッドのオーバーライド）
 	void Setter(int hp, int ap) override;
+	int Attack(double rate) override;
 	int Damaged(int damage) override;
 	bool CheckDead() override;
 
-	// 桃が攻撃を行うときの処理
-	int Attack();
-	// 桃が毒攻撃を行うときの処理
-	int PoisonAtk();
 	// 桃が防御を行うときの処理
 	void Defend();
-	// 桃が回復を行うときの処理
-	int Recover();
 	// 桃が防御状態かどうかを確認する処理
 	bool CheckDefend();
 	// 桃が恐怖状態になるときの処理
@@ -28,10 +24,10 @@ public:
 	// 鬼の毒状態が消えたかどうかを確認する処理
 	bool RemoveFrightened();
 private:
-	// 恐怖状態の残りターン数
-	int frightenedTurn;
 	// 桃の防御状態を表すフラグ定数
 	unsigned int isDefend = (1 << 1);
 	// 桃の恐怖状態を表すフラグ定数
 	unsigned int isFrightened = (1 << 2);
+	// 恐怖状態の残りターン数
+	int frightenedTurn;
 };
